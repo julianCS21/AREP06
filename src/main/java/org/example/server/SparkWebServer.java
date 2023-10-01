55ae79ed7e06fc8eaf1bff68c33da0d9be60bc0e
unknown <jul.1012@hotmail.com>
2023-10-01 13:56:11 -0500

    finished logservices

---
+++ src/main/java/org/example/server/SparkWebServer.java
@@ -0,0 +1,31 @@
+package org.example.server; /**
+ * Hello world!
+ *
+ */
+
+
+import org.example.connection.MongoDBConnection;
+
+import static spark.Spark.*;
+
+
+
+public class SparkWebServer {
+
+    public static void main(String... args){
+        port(getPort());
+        get("/logs", (req,res) -> {
+            String log = req.queryParams("log");
+            MongoDBConnection.insertLog(log);
+            return MongoDBConnection.getLast10Logs();
+        });
+    }
+
+    private static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4568;
+    }
+
+}
